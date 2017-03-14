e991bc7a1f8b7520240a54846c45d16a25244a91
John Cardenas <john.cardenas@cpe.pe>
2017-03-14 01:13:11 -0500

    Move: Folder test/sum to test/adhoc/sum

--- test/adhoc/suma/suma.spec.js
+++ test/adhoc/suma/suma.spec.js
@@ -1,6 +1,6 @@
 'use strict';
 var assert = require('assert');
-var sum = require('../../src/adhoc/sum').sum;
+var sum = require('../../../src/adhoc/sum').sum;
 describe('Adhoc', function() {
   describe('sum()', function() {
     it('should return 12 as sum of 8 and 4', function() {
